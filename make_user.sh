61df95d7a502e8318c4c7c350f97e08d8236a2c4
timofmax <timofmax@gmail.com>
2020-07-12 17:09:14 +0200

    minor changes

--- make_user.sh
+++ make_user.sh
@@ -6,23 +6,24 @@ if [[ "${UID}" -eq 0 ]]
 then 
     echo "Script run as root"
 else
-    echo "Run script as root"
+    echo "please run script as root"
     exit 1
 fi
 
 USER_NAME="${1}"
-COMMENT_NAME="${2}"
-COMMENT_SUR="${3}"
+shift
+COMMENT_NAME="${*}"
+
 
 PASSWORD=$(date +%s%N | sha256sum | head -c 16)
 SPEC_CHARACTER=$(echo "!@#$%^&*()_+=" | fold -w 1 | shuf | head -c 1)
 
 PASSWORD="${PASSWORD}${SPEC_CHARACTER}"
 
-useradd -c "${COMMENT_NAME} ${COMMENT_SUR}" -m "${USER_NAME}"
+useradd -c "${COMMENT_NAME}" -m "${USER_NAME}"
 echo "${PASSWORD}" | passwd --stdin "$USER_NAME"
 passwd -e "${USER_NAME}"
 
-echo "Successfully created ${USER_NAME} for ${COMMENT_NAME} ${COMMENT_SUR}"
+echo "Successfully created ${USER_NAME} for ${COMMENT_NAME}"
 echo "Password: ${PASSWORD}"
 
